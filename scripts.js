7695b372dbbddc93a08896f19fafc9e2ce44ddf1
manojPragada <pragadasaimanoj@gmail.com>
2024-02-13 08:30:44 -0500

    completed first section

--- scripts.js
+++ scripts.js
@@ -1,7 +1,14 @@
 var matrixRegex = /matrix\((-?\d*\.?\d+),\s*0,\s*0,\s*(-?\d*\.?\d+),\s*0,\s*0\)/;
+var section1Height, screenPos;
+
+$(window).on('load', function(){
+    section1Height = document.getElementById("section-1").clientHeight;
+    document.getElementById("myName").style.marginTop = (section1Height/2) + "px";
+});
+
 $(document).scroll(function () {
     var screenHeight = window.innerHeight;
-    var screenPos = window.scrollY;
+    screenPos = window.scrollY;
     var ele = document.querySelector("#section-1 #backdrop");
     if ((screenHeight / 2) > screenPos) {
         var currentScale = 1;
@@ -9,11 +16,16 @@ $(document).scroll(function () {
         var increment = screenPos / 500;
         currentScale = currentScale + increment;
         ele.style.filter = 'grayscale(0%)';
+        document.getElementById("myName").style.color = "black";
         ele.style.transform = 'scale(' + currentScale + ')';
-    } else if((screenHeight / 1.2) < screenPos){
-        var increment = screenPos / 500;
-        currentScale = currentScale + increment;
-        document.getElementById("myName").style.marginTop = currentScale+"%";
+        document.getElementById("myName").style.transform = 'scale(' + ((currentScale > 1.4)?currentScale-0.4:1) + ')';
+    } else if ((screenHeight / 1.2) < screenPos) {
         ele.style.filter = 'grayscale(100%)';
+        document.getElementById("myName").style.color = "white";
     }
+    var nameScale = (section1Height/2) + (screenPos / 2);
+    if (nameScale > (section1Height - 100)) {
+        nameScale = section1Height - 100;
+    }
+    document.getElementById("myName").style.marginTop = nameScale + "px";   
 });
